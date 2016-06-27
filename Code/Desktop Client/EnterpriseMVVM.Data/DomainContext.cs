3301ac73bed270c1fc081283b561410214c3e185
Van <van.le@pridetek.com>
2016-06-27 12:38:38 -0400

    Add Database DomainContext

---
+++ Code/Desktop Client/EnterpriseMVVM.Data/DomainContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseMVVM.Data
+{
+    public class DomainContext: DbContext
+    {
+
+        public DomainContext(): base("Default")//look for connection string "Default" from the base constructor
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
